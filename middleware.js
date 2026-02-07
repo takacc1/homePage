ab463fa763fc4c831df8f90cfb935c7868675004
takacc1 <takataka2006ta@gmail.com>
2026-02-07 20:15:41 +0900

    secure change

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
+import { NextResponse } from "next/server";
+
 export const config = {
-    matcher: '/(.*)',
+    matcher: "/(.*)",
 };
 
 export default function middleware(request) {
@@ -13,16 +15,19 @@ export default function middleware(request) {
             user === process.env.BASIC_AUTH_USER &&
             pass === process.env.BASIC_AUTH_PASSWORD
         ) {
-            // 認証成功 → 何も返さず処理を通過
-            return; // ← これでOK
+            // 認証成功 → 通過
+            return NextResponse.next();
         }
     }
 
-    // 認証失敗 → Basic認証ダイアログを表示
-    return new Response("認証が必要です。\nログインフォームが出ない場合は、Google Chrome等の外部サイトを使用してください。", {
-        status: 401,
-        headers: {
-            "WWW-Authenticate": 'Basic realm="Secure Area"',
-        },
-    });
+    // 認証失敗 → Basic認証ダイアログ
+    return new Response(
+        "認証が必要です。\nログインフォームが出ない場合は、Google Chrome等の外部サイトを使用してください。",
+        {
+            status: 401,
+            headers: {
+                "WWW-Authenticate": 'Basic realm="Secure Area"',
+            },
+        }
+    );
 }
